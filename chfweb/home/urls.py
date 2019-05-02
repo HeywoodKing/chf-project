5405bb9e91e4a23757aa2f50c68d7147b03b5cc3
flack <opencoding@hotmail.com>
2019-05-02 14:30:55 +0800

    add chfweb

---
+++ chfweb/home/urls.py
@@ -0,0 +1,16 @@
+# !-*- coding: utf-8 -*-
+# @Author  : ching(opencoding@hotmail.com)
+# @Date    : 2019/05/02
+# @Link    : www.cnblogs.com/ching126/ or blog.csdn.net/chenhongwu666
+# @Version : 
+# @Desc    :
+
+from django.urls import path
+from django.conf.urls import url
+from home import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('home', views.index, name='index'),
+]
